ed49ef6208be946e12ed76ee0e3ab410ee9fbab9
sDesmecht <samuel.desmecht@gmail.com>
2014-11-19 13:53:48 -0500

    Ajout class Personne (incomplet)

---
+++ Club de go Senthé/Employé.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Club_de_go_Senthé
+{
+    public class Employé:Personne
+    {
+      
+        #region CHAMPS & PROPRIÉTÉ
+        
+        private bool m_estSuperEmploye;
+        
+        public bool EstSuperEmploye
+        {
+            get { return m_estSuperEmploye; }
+        }
+        
+        #endregion
+
+        #region CONSTRUCTEUR 
+        public Employé(string pPrenom, string pNom, string pNumTelephone, string pAdCourriel, string pAdresse)
+            : base(pPrenom, pNom, pNumTelephone, pAdCourriel, pAdresse)
+        {
+
+        }
+              
+        #endregion
+
+        #region MÉTHODE
+        public static List<Employé> ChargerTousLesEmployes()
+        {
+
+        }
+
+        #endregion
+    }
+}
